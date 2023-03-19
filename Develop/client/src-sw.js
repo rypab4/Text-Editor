226acfeb65a622024f64a3f1070a85a16cd85ebe
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-19 16:39:04 -0700

    fixed src because there is no fallback file

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,11 +1,9 @@
-const { warmStrategyCache } = require('workbox-recipes');
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-const { OfflineFallback } = require('workbox-fallback-strategies');
-
 // The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -26,23 +24,7 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-//Set up page cache fallback
-const offlineFallback = new OfflineFallback({
-  fallbackURL: '/offline.html',
-});
-registerRoute(
-  ({request}) => request.mode === 'navigate',
-  async (args) => {
-    try {
-      return await pageCache.handle(args);
-    } catch (error) {
-      return await offlineFallback.handle(args);
-    }
-  }
-);
-
-
-// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // Set up asset cache
 //Implement asset caching

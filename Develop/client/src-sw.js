55cde689b4d91f1dc396394a2940f6f4945df62a
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-19 16:28:26 -0700

    updated the src, database, install and webpack

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -1,10 +1,12 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { OfflineFallback } = require('workbox-fallback-strategies');
 
+// The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
@@ -24,7 +26,44 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+//Set up page cache fallback
+const offlineFallback = new OfflineFallback({
+  fallbackURL: '/offline.html',
+});
+registerRoute(
+  ({request}) => request.mode === 'navigate',
+  async (args) => {
+    try {
+      return await pageCache.handle(args);
+    } catch (error) {
+      return await offlineFallback.handle(args);
+    }
+  }
+);
 
-// TODO: Implement asset caching
-registerRoute();
+
+// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// Set up asset cache
+//Implement asset caching
+registerRoute(
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
+      })
+    ],
+  })
+);
+
+
+
--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
 
   console.log('PUT to the database');
@@ -21,7 +21,7 @@ export const putDb = async (content) => {
 const contactDb = await openDB('jate', 1);
 
 // Create a new transaction and specify the database and data privileges.
-const tx = contactDb.transaction('jate', 'readonly');
+const tx = contactDb.transaction('jate', 'readwrite');
 
 // Open up the desired object store.
 const store = tx.objectStore('jate');

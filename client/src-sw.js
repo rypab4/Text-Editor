83db9c3ca52e0c232be82fa31ca6f0543490ba8d
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-19 19:00:15 -0700

    getting ready to push to heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -6,7 +6,7 @@ const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 // The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 
-console.log("--------client----------")
+
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
